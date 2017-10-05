e1a18a0c9164a01182873595566574fa281a57d1
Incrested <30969346+Incrested@users.noreply.github.com>
2017-10-05 08:56:05 -0700

    Create WallpaperChanger.ps1

---
+++ WallpaperChanger.ps1
@@ -0,0 +1,12 @@
+ cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1024x576.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1024x600.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1024x768.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1280x1024.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1280x768.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1280x800.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1360x768.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1366x768.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1440x900.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1680x1050.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1920x1080.jpg
+cat H:\Desktop\Changer\lord.png > C:\sdcs_tools\Backgrounds\Background1920x1200.jpg 
